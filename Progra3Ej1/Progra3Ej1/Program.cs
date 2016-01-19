39a66f2cad0ce416ae3762736322f3523298f603
chavy25 <chavy.abarca@gmail.com>
2016-01-18 21:22:30 -0600

    Version 1.0
    Inicial mensaje de Hola Mundo c#

---
+++ Progra3Ej1/Progra3Ej1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra3Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n\n************* Andres Abarca *************");
+            Console.ReadKey();
+
+        }
+    }
+}
